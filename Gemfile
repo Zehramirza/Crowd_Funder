2f65dff773c8ace0fc147fe6a0f906de28995ea5
Zehramirza <zehrabeg@gmail.com>
2013-07-18 15:51:19 -0400

    Making tests for browsing projects pass

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ gem 'rails', '3.2.13'
 
 gem 'pg' # replacing SQLite w/ PostgreSQL
 gem 'sorcery'
+gem 'thin'
 
 # Gems used only for assets and not required
 # in production environments by default.
---
+++ app/controllers/projects_controller.rb
@@ -0,0 +1,30 @@
+class ProjectsController < ApplicationController
+
+def index
+	@project = Project.all
+	@poop = 'this is poop'
+end
+
+def show 
+end
+
+def new
+
+end
+
+def edit
+
+end 
+
+def update 
+
+end
+
+def create
+end
+
+
+def destroy
+end
+
+end
