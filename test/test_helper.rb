d324748c4cd5ead7fab25893be7d70d1cc315f9b
Zehramirza <zehrabeg@gmail.com>
2013-07-19 17:35:40 -0400

    Writing tests for user registration

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,16 +6,20 @@ class ActiveSupport::TestCase
 end
 
 class ActionDispatch::IntegrationTest
-  # Make the Capybara DSL available in all integration tests
- include Capybara::DSL
+	  # Make the Capybara DSL available in all integration tests
+	 include Capybara::DSL
 
-  # Crowdfunder is to be changed to the name of your app
-  Capybara.app = Crowdfunder::Application
-  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
-  #
-  # Note: You'll currently still have to declare fixtures explicitly in integration tests
-  # -- they do not yet inherit this setting
-  # fixtures :all
+	  # Crowdfunder is to be changed to the name of your app
+	  Capybara.app = Crowdfunder::Application
+	  # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
+	  #
+	  # Note: You'll currently still have to declare fixtures explicitly in integration tests
+	  # -- they do not yet inherit this setting
+	  # fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+	  # Add more helper methods to be used by all tests here...
+	  teardown do
+	  Capybara.reset_sessions!    # Forget the (simulated) browser state
+	  Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+	end
 end
